7198733a81f6ec3200268b93acd431bae6eb0267
Marcio Campos <marciodscampos@gmail.com>
2015-08-18 07:50:19 -0300

    added loops.py

---
+++ pyexemples/loop.py
@@ -0,0 +1,12 @@
+pedidos = [
+    {
+        'nome': 'mario',
+        'sabor': 'portuguesa'
+    },
+    {
+        'nome': 'marco',
+        'sabor': 'presunto'
+    }
+]
+for pedido in pedidos:
+    print (pedido['nome'], pedido['sabor'])
