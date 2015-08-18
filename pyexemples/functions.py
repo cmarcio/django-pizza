3b0f9f5acf52886cf1709ca6e38349f53c6efa19
Marcio Campos <marciodscampos@gmail.com>
2015-08-18 08:40:52 -0300

    function with template

--- pyexemples/functions.py
+++ pyexemples/functions.py
@@ -1,13 +1,17 @@
 pedidos = []
 
-def adiciona_pedido(nome, sabor):
+def adiciona_pedido(nome, sabor, observacao=None):
     pedido = {}
     pedido['nome'] = nome
     pedido['sabor'] = sabor
+    pedido['observacao'] = observacao
+    return pedido
 
-    pedidos.append(pedido)
+pedidos.append(adiciona_pedido('mario', 'pepperoni'))
+pedidos.append(adiciona_pedido('marco', 'presunto', 'dobro de presunto'))
 
-print(pedidos)
-adiciona_pedido('mario', 'pepperoni')
-adiciona_pedido('marco', 'presunto')
-print(pedidos)
+for pedido in pedidos:
+    template = 'Nome: {nome}\nSabor: {sabor}'
+    print(template.format(**pedido))
+    if pedido['observacao']:
+        print('observacao: {}'.format(pedido['observacao']))
