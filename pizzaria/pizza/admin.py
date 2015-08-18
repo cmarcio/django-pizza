53299e672bdec9d3ce39ffb79f6b09241d7ca18c
Marcio Campos <marciodscampos@gmail.com>
2015-08-18 12:37:47 -0300

    Added search

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Pizza
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+	search_fields = ['name', 'ingredients']
+	list_fields = ['name', 'ingredients']
+
+
+admin.site.register(Pizza, PizzaAdmin)
